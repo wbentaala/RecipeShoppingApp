088410dfd90759e5948cede3a1eb39c9090b73cd
wassim <wassim.bentaala@gmail.com>
2018-12-30 22:43:24 +0100

    add edit component and routing

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
-
-  constructor() { }
+  recipeId: number;
+  recipe: Recipe;
+  editMode: boolean = false;
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params)=>{
+      this.recipeId = params['id'];
+      this.editMode = this.recipeId!=null;
+      this.recipe = this.recipeService.getRecipe(this.recipeId);
+    });
   }
 
 }
